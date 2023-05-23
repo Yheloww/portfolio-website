220d16a9ce1b152874865dd1836f2ba078e55905
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-05-23 16:34:36 +0200

    setup for the website

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    
+</svelte:head>
+
+
+<nav>
+	<a href="/">accueil</a>
+	<a href="/projects">projects</a>
+	<a href="/contact">contacts</a>
+</nav>
+
+<slot/>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
