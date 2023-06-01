4e088f672b114ffc933d1e56c50a2e4558da5c86
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-06-01 15:46:34 +0200

    portfolio project implemented

--- my-app/src/routes/projects/+page.svelte
+++ my-app/src/routes/projects/+page.svelte
@@ -4,22 +4,24 @@
     import { fade, fly } from "svelte/transition";
     import { onMount } from "svelte";
 
-    onMount(()=> {
-        const cards = document.querySelectorAll(".card")
+    // onMount(()=> {
+    //     const cards = document.querySelectorAll(".card")
 
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                entry.target.classList.toggle("show", entry.isIntersecting)
-            })
-        }, {
-            threshold:1,
-        })
+    //     const observer = new IntersectionObserver(entries => {
+    //         entries.forEach(entry => {
+    //             entry.target.classList.toggle("show", entry.isIntersecting)
+    //         })
+    //     }, {
+    //         threshold:1,
+    //     })
 
-    cards.forEach(card => {
-            observer.observe(card)
-        })
+    // cards.forEach(card => {
+    //         observer.observe(card)
+    //     })
 
-    })
+    // })
+
+    let link = "https://yheloww.github.io/remakeHackaton/"
 
 </script>
 
@@ -34,9 +36,26 @@
     </div>
 </div>
 
-<Project/>
-<Project/>
-<Project/>
+<Project web_link={link} 
+         github_link={"https://github.com/Yheloww/remakeHackaton"}
+         title={"Education for climate"}
+         description={"scrolly telling web experience about the education for climate in belgium. Project made during the scrolly telling hackaton organised by rtbf. In collaboration with 3 other people from different background. This version is the polished version that i made to exercise my fronted and data viz skills."}
+         picture={"./image/hackaton.png"}
+         />
+
+<Project web_link={link} 
+        github_link={"https://github.com/Yheloww/remakeHackaton"}
+        title={"D3 project"}
+        description={"scrolly telling web experience about the education for climate in belgium. Project made during the scrolly telling hackaton organised by rtbf. In collaboration with 3 other people from different background. This version is the polished version that i made to exercise my fronted and data viz skills."}
+        picture={"./image/hackaton.png"}
+        />
+         
+<Project web_link={"https://prediction-mhn8.onrender.com/"} 
+        github_link={"https://github.com/Yheloww/real-estate-price-prediction"}
+        title={"real estate prediction"}
+        description={"Little application based on a machine learning algorithm. It predicts the potential price of your real estate by you entering some charachteristics"}
+        picture={"./image/predictor.png"}
+        />
 
 </div>
 
@@ -51,14 +70,14 @@
     <div class=cards>
         <div class="card show">
             <h1 class="title-dash">Restaurants</h1>
-            <p class="description">Décrire</p>
-            <img class="image"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe1yI0EHo1qg4pQB-Nt2KX5g3Dr_QkvbWaZQ&usqp=CAU" alt="">
+            <p class="description">This power bi dashboard is meant for people to find restaurants for their next trip in Europe. It has been thought as a survey and you can explore the various aspects of it as the price range the city where you want to go, the best ranked restaurants and so on ...</p>
+            <img class="image"src="./image/restaurant.png" alt="predictor">
             <button class="site">See dashboard</button>
         </div>
         <div class="card show">
             <h1 class="title-dash">Dragonyte</h1>
-            <p class="description">Décrire</p>
-            <img class="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe1yI0EHo1qg4pQB-Nt2KX5g3Dr_QkvbWaZQ&usqp=CAU" alt="">
+            <p class="description">This tableau dashboard is the result of a use case proposed by accenture during my becode training. It analyze the datas of Dragonyte brewery a fake clientThen we cleaned and analyze the datas to get some insights out. At the end was a presentation of our findings to accenture people.</p>
+            <img class="image" src="./image/dragonite.png" alt="tableau dashboard">
             <button class="site">See dashboard</button>
         </div>
     </div>
@@ -80,6 +99,9 @@
 
 <style>
 
+p {
+    font-size: 20px;
+}
 h1 {
     margin: 0rem;
 }
@@ -119,17 +141,14 @@ h1 {
     padding: 2rem;
     gap:2rem;
     opacity: 0;
+    justify-content: space-between;
 }
 
 .card.show{
     opacity: 1;
-    transition: all 1s ease;
+    transition: all 1s ease-in;
 }
 
-
-.card:hover {
-    background-color: #dddddd;
-}
 
 .cards {
     max-width: 60rem;
@@ -149,6 +168,22 @@ img {
 .site {
     align-self: center;
 }
+
+button {
+    border-radius: 0.5rem;
+    border: 0.7px solid #a5a5a5;
+    background-color: #dddddd;
+    margin-bottom: 0;
+    padding: 0.5rem;
+    font-family: 'Assistant', sans-serif;
+    font-size: 20px;   
+}
+
+button:hover {
+    background-color: var(--text-yellow);
+    transition: all 0.5s ease;
+}
+
 
 @media (min-width: 600px) {
 
