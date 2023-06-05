e1c76f090210e05933af47ba2f651bb094d904a8
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-06-05 10:14:46 +0200

    Updates

---
+++ _app/immutable/chunks/paths.cf9b9d6a.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1nvdz13)==null?void 0:s.base)??"/sveltekit-gh-pages";var e;const a=((e=globalThis.__sveltekit_1nvdz13)==null?void 0:e.assets)??t;export{a,t as b};
